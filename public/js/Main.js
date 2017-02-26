2b89e917b136ecc8dea2ec354dde02464ecaab80
Anthony <anthonysiu@gmail.com>
2017-02-26 11:43:49 -0600

    box2d start

--- public/js/Main.js
+++ public/js/Main.js
@@ -1,13 +1,22 @@
+var ctx;
+var background_clr = "#88B4FF";//"#00";//"#AA8844";
 (function(){
-    Renderer = new PIXI.WebGLRenderer(800, 200, {backgroundColor : 0x59b4ff, transparent : false, antialias: false});
-    $( "#canvas" )[0].appendChild(Renderer.view);
+    Renderer = new PIXI.WebGLRenderer(800, 800, {backgroundColor : 0x59b4ff, transparent : false, antialias: false});
+	var canvas1 = document.getElementById("canvas1");
+	canvas1.appendChild(Renderer.view);
     Stage0 = new PIXI.Container();
-
+	
+	
+	
     var initialize = function(load, res){
         loader = load;
         resources = res;
-        
-        
+		
+		var canvas = document.createElement('canvas');		
+		ctx = canvas.getContext('2d');
+		div = document.getElementById("canvas1"); 
+		div.appendChild(canvas);
+                
         updateQueue = new UpdateQueue();        
 
         animate();
@@ -22,7 +31,11 @@
     function animate() {
         update();
         requestAnimationFrame(animate);
-        Renderer.render(Stage0);        
+        Renderer.render(Stage0);  
+
+		ctx.rect(0,0,ctx.canvas.width,ctx.canvas.height);
+		ctx.fillStyle=background_clr;
+		ctx.fill();		
     } // end animate
 
     
